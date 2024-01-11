637bf995ee0cc40bd6c84e820b4cb9b84538e0d6
filipe.dias <fedispato@gmail.com>
2024-01-11 13:46:35 -0300

    feat: Adicionando partes de GET, POST, Service e conexao com banco de dados no projeto

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # kotlinSpring
 Projeto para estudo de Web Services utilizando Spring e Kotlin
+
+Adicionado listagem de mensagens
+Adicionado salvamento de nova mensagem
+Adicionado pesquisa de mensagem por ID
+Adicionado conexão com banco de dados
+Adicionado parte de serviços
--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -2,9 +2,11 @@ package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.query
+import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.*
+import java.util.UUID
 
 @SpringBootApplication
 class DemoApplication
@@ -14,9 +16,52 @@ fun main(args: Array<String>) {
 }
 
 @RestController
-class MessageController {
+class MessageController(val service: MessageService) {
+
+	@GetMapping("/index")
+	fun indexExample(@RequestParam("name") name: String) = "Hello, $name"
+
+	@GetMapping("/messagesExample")
+	fun messagesExample() = listOf(
+		Message("1", "Hello!"),
+		Message("2", "Bonjour!"),
+		Message("3", "Privet!"),
+	)
 
 	@GetMapping("/")
-	fun index(@RequestParam("name") name: String) = "Hello, $name"
+	fun index(): List<Message> = service.findMessages()
+
+	@GetMapping("/{id}")
+	fun index(@PathVariable id: String): List<Message> = service.findMessageById(id)
+
+	@PostMapping("/")
+	fun post(@RequestBody message: Message) {
+		service.save(message)
+	}
 
 }
+
+@Service
+class MessageService(val db: JdbcTemplate) {
+
+	fun findMessages(): List<Message> = db.query("select * from messages") {
+		response, _ -> Message(response.getString("id"), response.getString("text"))
+	}
+
+	fun save(message: Message) {
+		val id = message.id ?: UUID.randomUUID().toString()
+		db.update(
+			"insert into messages values ( ?, ?)",
+			id, message.text
+		)
+	}
+
+	fun findMessageById(id: String): List<Message> = db.query("select * from messages where id = ?", id) {
+		response, _ -> Message(response.getString("id"), response.getString("text"))
+	}
+
+}
+
+data class Message(val id: String?, val text: String)
+
+
